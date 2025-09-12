e50a0ac5c90a629fd7d9897e278133f0ec085b77
Harsh Shrivastava <harshshrivastava.work@gmail.com>
2025-09-13 00:27:11 +0530

    Initial commit

---
+++ backend/Kiraflow/src/main/java/com/example/Kiraflow/KiraflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kiraflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiraflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiraflowApplication.class, args);
+	}
+
+}
---
+++ backend/Kiraflow/src/test/java/com/example/Kiraflow/KiraflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kiraflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiraflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
